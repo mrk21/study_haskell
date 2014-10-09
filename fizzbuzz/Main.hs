6c3fd90a727c12239b5441007921bd4946e385e7
Yuichi Murata <mrk21info+github@gmail.com>
2014-10-09 10:43:12 +0900

    Fixed the fizzbuzz.

--- fizzbuzz/Main.hs
+++ fizzbuzz/Main.hs
@@ -1,12 +1,23 @@
-fizzbuzzImpl :: Int -> Int -> [String]
-fizzbuzzImpl n i | i > n           = []
-                 | (mod i 15) == 0 = "FizzBuzz" : (fizzbuzzImpl n (i+1))
-                 | (mod i  5) == 0 = "Buzz"     : (fizzbuzzImpl n (i+1))
-                 | (mod i  3) == 0 = "Fizz"     : (fizzbuzzImpl n (i+1))
-                 | otherwise       = (show i)   : (fizzbuzzImpl n (i+1))
-
 fizzbuzz :: Int -> [String]
-fizzbuzz n = fizzbuzzImpl n 1
+fizzbuzz n = impl n 1
+  where
+    impl m i
+        | i > m         = []
+        | mod i 15 == 0 = "FizzBuzz" : impl m (i+1)
+        | mod i  5 == 0 = "Buzz"     : impl m (i+1)
+        | mod i  3 == 0 = "Fizz"     : impl m (i+1)
+        | otherwise     = show i     : impl m (i+1)
+
+fizzbuzz2 :: Int -> [String]
+fizzbuzz2 n =
+    zipWith select (map show [1..n]) $
+    zipWith select (make 3 "Fizz") $
+    zipWith select (make 5 "Buzz") (make 15 "FizzBuzz")
+  where
+    make m s = concat . concat . repeat $ replicate (m-1) [""] ++ [[s]]
+    select a b = if null b then a else b
 
 main :: IO ()
-main = print $ fizzbuzz 100
+main = do
+    print $ fizzbuzz 100
+    print $ fizzbuzz2 100
